43c8d4e81a6003925fc7fd6672f99b92d4ae6dc4
sviat-l <sviatoslav.lushnei@ucu.edu.ua>
2024-05-25 02:17:39 +0300

    refactored cassandra popularting scripts

--- spark-programs/kafka-cassandra.py
+++ spark-programs/kafka-cassandra.py
@@ -1,9 +1,10 @@
 import argparse
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.streaming import StreamingQuery
+import pyspark.sql.functions as F
 
-TABLES_CONFIG ={
-  "domain_articles": {
+CASSANDRA_TABLES_CONFIG ={
+  "domain_pages": {
     "columns": ["domain", "page_id"],
   },
   "user_pages": {
@@ -21,9 +22,9 @@ TABLES_CONFIG ={
 }
 
 def create_spark_session(app_name: str, master: str, log_level: str, 
-                         cassandra_host: str="cassandra", cassandra_port: str="9042",
-                         cores_max:int=1, executor_mem:str='1g') -> SparkSession:
-    """Creating the Spark session with the given configurations."""
+                         cassandra_host: str="cassandra", cassandra_port: str="9042"
+                         ) -> SparkSession:
+    """ Creating the Spark session with the given configurations."""
     spark_session = SparkSession \
             .builder \
             .appName(app_name) \
@@ -33,67 +34,66 @@ def create_spark_session(app_name: str, master: str, log_level: str,
             .config("spark.sql.shuffle.partitions", 4) \
             .config("spark.cassandra.connection.host", cassandra_host) \
             .config("spark.cassandra.connection.port", cassandra_port) \
-            .config("spark.executor.memory", executor_mem) \
-            .config("spark.cores.max", cores_max) \
             .master(master) \
             .getOrCreate()
     spark_session.sparkContext.setLogLevel(log_level)
     return spark_session
 
 def read_kafka_stream(spark: SparkSession, bootstrap_servers: str, topic: str) -> DataFrame:
-    """Read messages from Kafka topic as a stream."""
+    """ Read messages from Kafka topic as a stream."""
     return spark.readStream \
             .format("kafka") \
             .option("kafka.bootstrap.servers", bootstrap_servers) \
             .option("subscribe", topic) \
             .load()
             
-def write_to_cassandra(df: DataFrame, keyspace: str, table: str) -> StreamingQuery:
-    """Write messages to Cassandra table as a stream."""
-    df = df.select("request_id", "user_id", "domain", 
-                       "created_at", "page_title")
-    return df.writeStream \
-            .foreachBatch(lambda batch_df, batch_id: 
-                batch_df.write \
-                    .format("org.apache.spark.sql.cassandra") \
-                    .options(table=table,keyspace=keyspace
-                    ).mode("append").save()
-            ).start()
-            
-def save_to_cassandra_table(df: DataFrame, keyspace: str, table: str):
+def append_to_cassandra_table(df: DataFrame, keyspace: str, table: str):
     """ Write data to a Cassandra table."""
     df.write \
         .format("org.apache.spark.sql.cassandra") \
         .options(table=table,keyspace=keyspace
         ).mode("append").save()
             
-def write_all_to_cassandra(df: DataFrame, df_id, keyspace: str='wiki'):
+def write_all_to_cassandra(df: DataFrame, df_id:DataFrame, keyspace: str='wiki'):
     """ Write data to all tables in the keyspace."""
-    for table, config in TABLES_CONFIG.items():
-        table_df = df.select(config["columns"])
-        table_df = table_df.filter(table_df.columns[0] + " IS NOT NULL")
-        save_to_cassandra_table(table_df, keyspace, table)
-
+    for table, config in CASSANDRA_TABLES_CONFIG.items():
+        batch_df = df.select(config["columns"])
+        batch_df = batch_df.filter(batch_df.columns[0] + " IS NOT NULL")
+        append_to_cassandra_table(batch_df, keyspace, table)
     
 def start_cassandra_write(df: DataFrame) -> StreamingQuery:
+    """ Start writing the data to Cassandra."""
     return df.writeStream \
         .foreachBatch(write_all_to_cassandra) \
         .start()
 
 def preprocess_data(input_df: DataFrame) -> DataFrame:
-    """Process the input data and return in the format to write to Cassandra."""
-    data_df = input_df.selectExpr("CAST(value AS STRING)")
-    parsed_df = data_df\
-            .selectExpr("from_json(value, 'struct<"+
-                "domain:string, "+ 
-                "request_id:string, "+
-                "user_id:integer, "+
-                "user_text:string, "+
-                "user_is_bot:boolean, "+
-                "created_at:string, "+
-                "page_id:integer,"
-                "page_title:string>') AS data")\
-            .select("data.*")
+    """ Process the input data and return in the format to write to Cassandra."""
+    # Selecting the data from the input stream message
+    filtered_df = input_df\
+        .selectExpr("CAST(value AS STRING)")\
+        .filter(F.expr("value LIKE 'data: %'"))\
+        .select(F.expr("substring(value, 7) AS data"))
+    # Parsing the JSON data BASED on the data schema
+    data_df = filtered_df\
+        .selectExpr("from_json(data, 'struct<"+
+            "meta:struct<domain:string, request_id:string>, "+
+            "performer:struct<user_text:string, user_is_bot:boolean, user_id:integer>, "+
+            "dt:string, "+
+            "page_id:integer,"
+            "page_title:string>') AS data")\
+        .select("data.*")
+    # Formatting to the proper schema to write to Cassandra
+    parsed_df = data_df.select(
+        F.col("meta.domain").alias("domain"),
+        F.col("meta.request_id").alias("request_id"),
+        F.col("performer.user_id").alias("user_id"),
+        F.col("performer.user_text").alias("user_text"),
+        F.col("performer.user_is_bot").alias("user_is_bot"),
+        F.col("dt").alias("created_at"),
+        F.col("page_title"),
+        F.col("page_id"),
+    )
     return parsed_df
 
 
@@ -102,18 +102,15 @@ def main(args: argparse.Namespace):
     Args:
     args.boostrap_servers: Kafka bootstrap servers
     args.read_topic: Kafka topic name to read messages from
-    args.write_topic: Kafka topic name to write filtered messages to
     args.master: Spark master URL
     args.app_name: Name of the Spark application
     args.log_level: Log level for the Spark context
-    args.cores_max: Maximum number of cores to use
-    args.executor_mem: Memory allocated to each executor
     args.cassandra_host: Cassandra host
     args.cassandra_port: Cassandra port
     args.keyspace: Cassandra keyspace write to
     """
     spark_session = create_spark_session(args.app_name, args.master, args.log_level, 
-                    args.cassandra_host, args.cassandra_port,args.cores_max, args.executor_mem)
+                    args.cassandra_host, args.cassandra_port)
     print("Reading from Kafka topic: ", args.read_topic)
     input_df = read_kafka_stream(spark_session, args.bootstrap_servers, args.read_topic)
     processed_df = preprocess_data(input_df)
@@ -128,7 +125,7 @@ if __name__ == "__main__":
         description='Read messages from Kafka topic and write to Cassandra.')
     argparser.add_argument('--bootstrap-servers', type=str,
                         default="kafka:9092", help='Kafka bootstrap servers')
-    argparser.add_argument('--read_topic', type=str, default="processed",
+    argparser.add_argument('--read_topic', type=str, default="input",
                         help='Kafka topic name to read messages from')
     argparser.add_argument("--master", type=str,
                            default="spark://spark-master-server:7077", help="Spark master URL")
@@ -136,8 +133,6 @@ if __name__ == "__main__":
                             default="Kafka-Spark-Cassandra", help="Name of the Spark application")
     argparser.add_argument("--log_level", type=str, default="WARN", choices=["WARN", "INFO", "DEBUG"],
                            help="Log level for the Spark context")
-    argparser.add_argument("--cores_max", type=int, default=1, help="Maximum number of cores to use")
-    argparser.add_argument("--executor_mem", type=str, default="1g", help="Memory allocated to each executor")
     argparser.add_argument("--cassandra_host", type=str, default="cassandra", help="Cassandra host")
     argparser.add_argument("--cassandra_port", type=str, default="9042", help="Cassandra port")
     argparser.add_argument("--keyspace", type=str, default="wiki", help="Cassandra keyspace")
