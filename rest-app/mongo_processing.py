311fd2064b993a416f0a5bd19e66d88cfd1dc979
nazar12314 <59284695+nazar12314@users.noreply.github.com>
2024-05-25 19:08:04 +0300

    added insert methods for mongodb client

--- rest-app/mongo_processing.py
+++ rest-app/mongo_processing.py
@@ -9,8 +9,8 @@ logging.basicConfig(level=logging.INFO, format='|%(asctime)s| - |%(name)s| - |%(
 logger = logging.getLogger(__name__)
 
 # Initialize MongoDB client
-MONGO_URL = "mongodb://localhost:27017"
-DB_NAME = "wikiData"
+MONGO_URL = 'mongodb://mongodb:27017'
+DB_NAME = 'wikiData'
 
 
 class MongoDBClient:
@@ -23,6 +23,27 @@ class MongoDBClient:
         self.db.domain_stats.create_index([("time_start", 1)])
         self.db.bot_creation_stats.create_index([("time_start", 1)])
         self.db.most_productive.create_index([("time_start", 1)])
+
+    def insert_domain_stats(self, domain_counts):
+        try:
+            self.db.domain_stats.insert_one(domain_counts)
+            logger.info("Domain stats inserted successfully.")
+        except Exception as e:
+            logger.error(f"Failed to insert domain stats: {str(e)}")
+
+    def insert_bot_creation_stats(self, bot_stats):
+        try:
+            self.db.bot_creation_stats.insert_one(bot_stats)
+            logger.info("Bot creation stats inserted successfully.")
+        except Exception as e:
+            logger.error(f"Failed to insert bot creation stats: {str(e)}")
+
+    def insert_most_productive(self, top_users):
+        try:
+            self.db.most_productive.insert_one(top_users)
+            logger.info("Most productive users stats inserted successfully.")
+        except Exception as e:
+            logger.error(f"Failed to insert top users stats: {str(e)}")
 
     def get_domain_stats(self) -> List[Dict]:
         try:
